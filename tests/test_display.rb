42bfc3083a53fc5e2c6f41604f5ebd9caeee856a
Craig Isenor <toast@Craigs-iMac.office.stembolt.io>
2018-03-16 10:14:43 -0700

    Removed unimportant tests

--- tests/test_display.rb
+++ tests/test_display.rb
@@ -3,19 +3,6 @@ require_relative '../views/console_view.rb'
 require_relative '../views/html_view.rb'
 # Class used for testing the month class.
 class TestDisplay < Test::Unit::TestCase
-  def test_private_methods
-    @display = ConsoleView.new
-    assert_raise(NoMethodError) do
-      @display.find_longest_month
-    end
-    assert_raise(NoMethodError) do
-      @display.justify
-    end
-    assert_raise(NoMethodError) do
-      @display.bold_if_holiday
-    end
-  end
-
   def test_html_display_with_id_and_class
     test = HTMLMarkup.new.get_markup_block('content', 'test-class')
     assumed = '<div class="test-class">content</div>'

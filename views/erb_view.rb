ecea3f9f7275b31e98be88d3ae5ff6a03bc07bd9
Craig Isenor <toast@Craigs-iMac.office.stembolt.io>
2018-03-16 13:43:06 -0700

    Refactor the init params.

--- views/erb_view.rb
+++ views/erb_view.rb
@@ -1,17 +1,18 @@
 require 'erb'
-require_relative '../utility/log'
+require_relative '../utility/console_log'
 
 class PrintToERB
-  def initialize(template_path, log = ConsoleLog.new)
+  def initialize(template_path, output_path, log = ConsoleLog.new)
     @log = log
     @template = File.read(template_path)
+    @output_path = output_path
   rescue IOError
     @log.error "Could not find template file at #{template_path}"
   end
 
-  def render(bind, dest)
+  def render(view_model)
     raise 'No ERB template is loaded.' unless @template
-    populated = ERB.new(@template).run(bind)
-    File.open(dest, 'w+', 0o644) { |file| file.puts populated }
+    populated = ERB.new(@template).result(view_model.get_binding)
+    File.open(@output_path, 'w+', 0o644) { |file| file.puts populated }
   end
 end
